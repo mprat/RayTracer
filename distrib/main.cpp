d04ba512f6d969984d3513225b090f4051ccfa84
mprat <michele.pratusevich@gmail.com>
2012-11-15 03:51:18 -0500

    first try at main

--- distrib/main.cpp
+++ distrib/main.cpp
@@ -18,7 +18,9 @@ float clampedDepth ( float depthInput, float depthMin , float depthMax);
 int main( int argc, char* argv[] )
 {
   // Fill in your implementation here.
-
+	int size_x, size_y;
+	char* filename; 
+	
   // This loop loops over each of the input arguments.
   // argNum is initialized to 1 because the first
   // "argument" provided to the program is actually the
@@ -26,6 +28,32 @@ int main( int argc, char* argv[] )
   for( int argNum = 1; argNum < argc; ++argNum )
     {
       std::cout << "Argument " << argNum << " is: " << argv[argNum] << std::endl;
+		if (strcmp(argv[argNum], "size") == 0){
+			if (argNum + 2 < argc){
+				if (isdigit(atoi(argv[argNum + 1]))){
+					size_x = atoi(argv[argNum + 1]);
+				} else {
+					cout<<"size in x must be a number"<<endl;
+					return 0;
+				}
+				if (isdigit(atoi(argv[argNum + 2]))){
+					size_y = atoi(argv[argNum + 2]);
+				} else {
+					cout<<"size in y must be a number"<<endl;
+					return 0;
+				}
+			} else {
+				cout<< "Not enough size arguments!"<<endl;
+				return 0;
+			}
+		} else if (strcmp(argv[argNum], "-input")){
+			if (argNum + 1 < argc){
+				filename = argv[argNum + 1];
+			} else {
+				cout<<"not enough filename arguments"<<endl;
+				return 0;
+			}
+		}
     }
 	
     
@@ -35,7 +63,8 @@ int main( int argc, char* argv[] )
   // the scene.  Write the color at the intersection to that
   // pixel in your output image.
 
-
+	SceneParser parser = SceneParser(filename);
+		
 
  
   ///TODO: below demonstrates how to use the provided Image class
