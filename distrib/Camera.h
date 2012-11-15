9fb38ca9c4b7250291438e58b76d85eb0739002c
mprat <michele.pratusevich@gmail.com>
2012-11-15 03:29:13 -0500

    perspective camera

--- distrib/Camera.h
+++ distrib/Camera.h
@@ -16,24 +16,32 @@ public:
 	virtual float getTMin() const = 0 ; 
 	virtual ~Camera(){}
 protected:
-	Vector3f center; 
-	Vector3f direction;
-	Vector3f up;
-	Vector3f horizontal;
-
+//	Vector3f center; 
+//	Vector3f direction;
+//	Vector3f up;
+//	Vector3f horizontal;
+	Vector3f u;
+	Vector3f v;
+	Vector3f w;
+	Vector3f center;
+	float angle;
+	double D;
 };
 
-///TODO: Implement Perspective camera
-///Fill in functions and add more fields if necessary
 class PerspectiveCamera: public Camera
 {
 public:
 	PerspectiveCamera(const Vector3f& center, const Vector3f& direction,const Vector3f& up , float angle){
-
+		this->center = center;
+		this->w = direction;
+		this->u = (Vector3f::cross(this->w, up)).normalized();
+		this->v = (Vector3f::cross(this->u, this->w)).normalized();
+		this->angle = angle;
+		this->D = 1 / tan(angle / 2) ;
 	}
 
 	virtual Ray generateRay( const Vector2f& point){
-		
+		return Ray(this->center, (point.x * this->u + point.y*this->v + D * this->w).normalized());	
 	}
 
 	virtual float getTMin() const { 
