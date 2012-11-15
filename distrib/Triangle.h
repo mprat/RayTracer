a71f2ab262c6598a76433c334eea7c2ab0e7d102
mprat <michele.pratusevich@gmail.com>
2012-11-15 13:55:50 -0500

    triangles complete except for normal

--- distrib/Triangle.h
+++ distrib/Triangle.h
@@ -7,10 +7,6 @@
 #include <iostream>
 
 using namespace std;
-///TODO: implement this class.
-///Add more fields as necessary,
-///but do not remove hasTex, normals or texCoords
-///they are filled in by other components
 class Triangle: public Object3D
 {
 public:
@@ -18,16 +14,37 @@ public:
         ///@param a b c are three vertex positions of the triangle
 	Triangle( const Vector3f& a, const Vector3f& b, const Vector3f& c, Material* m):Object3D(m){
           hasTex = false;
+		this->a = a;
+		this->b = b;
+		this->c = c;
 	}
 
 	virtual bool intersect( const Ray& ray,  Hit& hit , float tmin){
-		
+		Matrix3f A(a - b, a - c, ray.getDirection(), true);
+		Vector3f col = a - ray.getOrigin();	
+		float detA = A.determinant();
+		Matrix3f A_beta(A);
+		A_beta.setCol(0, col);
+		float beta = A_beta.determinant() / detA;
+		Matrix3f A_gamma(A);
+		A_gamma.setCol(1, col);
+		float gamma = A_gamma.determinant() / detA;
+		Matrix3f A_t(A);
+		A_t.setCol(2, col);
+		float t = A_t.determinant() / detA;
+		if (t >= tmin && t < hit.getT() && beta + gamma <= 1.0 && beta >= 0 && gamma >= 0){
+			hit.set(t, this->material, alpha *normals[0] + beta *normals[1] + gamma * normals[2]);
+			return true;
+		}	
+		return false;
 	}
 	bool hasTex;
 	Vector3f normals[3];
 	Vector2f texCoords[3];
 protected:
-
+	Vector3f a;
+	Vector3f b;
+	Vector3f c;
 };
 
 #endif //TRIANGLE_H
